3a690ce2a41fc4bf8c32791d3d53b1306fb2b42b
nkomiljon <komiljonnajmitdinov99@gmail.com>
2020-09-25 16:52:06 +0500

    Начальная версия

---
+++ pkg/types/types.go
@@ -0,0 +1,23 @@
+package types
+
+type Money int64
+
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+type PAN string
+
+type Card struct {
+	ID        int
+	PAN       string
+	Balance   Money
+	Currency  Currency
+	Color     string
+	Name      string
+	Active    bool
+}
